2fb8c3f1324b703c82ef0cd51a3c91d2a8dfa41b
Bonevy BEBY <bonevybeby@gmail.com>
2023-11-20 18:04:25 +0100

    first commit

---
+++ src/main/java/com/formationkilo/jwtservicespringsecurity6/Jwtservicespringsecurity6Application.java
@@ -0,0 +1,13 @@
+package com.formationkilo.jwtservicespringsecurity6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jwtservicespringsecurity6Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jwtservicespringsecurity6Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/formationkilo/jwtservicespringsecurity6/Jwtservicespringsecurity6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.formationkilo.jwtservicespringsecurity6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jwtservicespringsecurity6ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
